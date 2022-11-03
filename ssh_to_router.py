e83b92b2052ff2b0cff366c61dc3ce08e679b6a7
jsamo <jsamo25@gmail.com>
2022-11-03 09:20:31 +0100

    init example

---
+++ ssh_to_router.py
@@ -0,0 +1,22 @@
+from netmiko import ConnectHandler
+
+#Device object where you want to connect
+
+device_id ={
+    'device_type' : 'cisco_ios', #check CLASS_MAPPER in https://github.com/ktbyers/netmiko/blob/master/netmiko/ssh_dispatcher.py
+    'host':'192.168.0.1',
+    'username': 'cisco',
+    'password': 'cisco',
+    'port': 8022,  # optional, defaults to 22
+    'secret': 'secret',  # optional, defaults to ''
+}
+
+#Start connection to device
+net_connect = ConnectHandler(**device_id)
+
+#Commands can be executed
+output = net_connect.send_command('show ver | inc IOS')
+print("the current IOS version is {}".format(output))
+
+#Close connection
+net_connect.disconnect()
